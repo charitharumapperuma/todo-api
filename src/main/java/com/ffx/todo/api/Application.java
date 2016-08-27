c3c2d31aa0aa4b5f505fe9f7bb05f456a506e6e9
Charith Arumapperuma <charithk44@gmail.com>
2016-08-28 00:15:34 +0530

    added Application class and its functionality

---
+++ src/main/java/com/ffx/todo/api/Application.java
@@ -0,0 +1,14 @@
+package com.ffx.todo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Charith on 8/28/2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
